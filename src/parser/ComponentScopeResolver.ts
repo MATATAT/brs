0414e95f60bb09f41b5a3eb7883e20c74e3b3734
Matt Macdonald <matt.macdonald@hulu.com>
2019-12-09 17:45:29 -0800

    set up failing test

--- src/parser/ComponentScopeResolver.ts
+++ src/parser/ComponentScopeResolver.ts
@@ -1,5 +1,21 @@
+import { ComponentDefinition } from "../componentprocessor";
+import { Statement } from "./Statement";
+
 export class ComponentScopeResolver {
     constructor() {
         // Do the thing
+    }
+
+    /**
+     * Resolves the component functions in scope based on the extends hierarchy.
+     * @param component Instance of the component to resolve function scope for.
+     * @param componentMap Component definition map to reference for function resolution of given component.
+     * @returns All the statements
+     */
+    public resolve(
+        component: ComponentDefinition,
+        componentMap: Map<string, ComponentDefinition>
+    ): Statement[] {
+        return [];
     }
 }
--- test/parser/ComponentScopeResolver.test.js
+++ test/parser/ComponentScopeResolver.test.js
@@ -1,8 +1,37 @@
 const brs = require("brs");
+const { getComponentDefinitionMap } = require("../../lib/componentprocessor");
+const path = require("path");
+
+jest.mock("fast-glob");
+jest.mock("fs");
+const fg = require("fast-glob");
+const fs = require("fs");
+
+const realFs = jest.requireActual("fs");
 
 describe("ComponentScopeResolver", () => {
-    it("is a test", () => {
+    beforeEach(() => {
+        fg.sync.mockImplementation(() => {
+            return [
+                "baseComponent.xml",
+                "extendedComponent.xml",
+                "scripts/baseComponent.brs",
+                "scripts/extendedComponenet.brs",
+            ];
+        });
+        fs.readFile.mockImplementation((filename, _, cb) => {
+            resourcePath = path.join(__dirname, "resources", filename);
+            realFs.readFile(resourcePath, (err, contents) => {
+                cb(/* no error */ null, contents);
+            });
+        });
+    });
+
+    test("resolving function scope across two components", async () => {
+        let componentMap = await getComponentDefinitionMap("/doesnt/matter");
+        let componentToResolve = componentMap.get("ExtendedComponent");
         let componentScopeResolver = new brs.parser.ComponentScopeResolver();
-        expect(componentScopeResolver).not.toBeUndefined();
+        let statements = componentScopeResolver.resolve(componentToResolve, componentMap);
+        expect(statements.length).toBeGreaterThan(0);
     });
 });

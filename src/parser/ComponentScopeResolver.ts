505108d865fb1909a0fa659e8c5212a824487e7a
Matt Macdonald <matt.macdonald@hulu.com>
2019-12-10 15:51:12 -0800

    non-passing tests but merging functions

--- src/parser/ComponentScopeResolver.ts
+++ src/parser/ComponentScopeResolver.ts
@@ -1,21 +1,52 @@
 import { ComponentDefinition } from "../componentprocessor";
-import { Statement } from "./Statement";
+import * as Stmt from "./Statement";
 
 export class ComponentScopeResolver {
-    constructor() {
-        // Do the thing
-    }
+    /**
+     * @param componentMap Component definition map to reference for function resolution.
+     * @param parserLexerFn Function used to parse statements out of given components
+     */
+    constructor(
+        readonly componentMap: Map<string, ComponentDefinition>,
+        readonly parserLexerFn: (filenames: string[]) => Promise<Stmt.Statement[]> // TODO: Remove and just build here?
+    ) {}
 
     /**
      * Resolves the component functions in scope based on the extends hierarchy.
      * @param component Instance of the component to resolve function scope for.
-     * @param componentMap Component definition map to reference for function resolution of given component.
-     * @returns All the statements
+     * @returns All statements in scope for the resolved component
      */
-    public resolve(
-        component: ComponentDefinition,
-        componentMap: Map<string, ComponentDefinition>
-    ): Statement[] {
-        return [];
+    public async resolve(component: ComponentDefinition): Promise<Stmt.Statement[]> {
+        return Promise.all(this.getStatements(component)).then(this.flatten);
+    }
+
+    private flatten(statementMap: Stmt.Statement[][]): Stmt.Statement[] {
+        let statements = statementMap.shift() || [];
+        while (statementMap.length > 0) {
+            let extendedFns = statementMap.shift() || [];
+            statements = statements.concat(
+                extendedFns.filter(statement => {
+                    return statement instanceof Stmt.Function;
+                })
+            );
+        }
+        return statements;
+    }
+
+    private *getStatements(component: ComponentDefinition) {
+        yield this.parserLexerFn(component.scripts.map(c => c.uri));
+        let currentComponent: ComponentDefinition | undefined = component;
+        while (currentComponent.extends) {
+            let previousComponent = currentComponent;
+            currentComponent = this.componentMap.get(currentComponent.extends);
+            if (!currentComponent) {
+                return Promise.reject({
+                    message: `Cannot find extended component ${previousComponent.extends} defined on ${previousComponent.name}`,
+                });
+            }
+            yield this.parserLexerFn(currentComponent.scripts.map(c => c.uri));
+        }
+
+        return Promise.resolve();
     }
 }

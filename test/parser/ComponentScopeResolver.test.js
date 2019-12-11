6f72e52f2568d371ba85e12ef3d66eb246739c3e
Matt Macdonald <matt.macdonald@hulu.com>
2019-12-11 11:53:40 -0800

    Adding extends tests with added scripts

--- test/parser/ComponentScopeResolver.test.js
+++ test/parser/ComponentScopeResolver.test.js
@@ -12,19 +12,35 @@ const fs = require("fs");
 const realFs = jest.requireActual("fs");
 
 describe("ComponentScopeResolver", () => {
-    beforeEach(() => {
+    let parseFn, componentMap;
+
+    beforeEach(async () => {
         fg.sync.mockImplementation(() => {
             return [
                 "baseComponent.xml",
                 "extendedComponent.xml",
+                "baseComponentTwoScripts.xml",
+                "extendedComponent2.xml",
                 "scripts/baseComponent.brs",
                 "scripts/extendedComponenet.brs",
+                "scripts/utilityBase.brs",
+                "scripts/utilityExtended.brs",
             ];
         });
         fs.readFile.mockImplementation((filename, _, cb) => {
             resourcePath = path.join(__dirname, "resources", filename);
             realFs.readFile(resourcePath, "utf8", (err, contents) => {
                 cb(/* no error */ null, contents);
+            });
+        });
+
+        parseFn = LexerParser.getLexerParserFn(new Map(), defaultExecutionOptions);
+
+        componentMap = await getComponentDefinitionMap("/doesnt/matter");
+        componentMap.forEach(comp => {
+            comp.scripts = comp.scripts.map(script => {
+                script.uri = path.join("scripts/", path.parse(script.uri).base);
+                return script;
             });
         });
     });
@@ -35,16 +51,6 @@ describe("ComponentScopeResolver", () => {
     });
 
     test("resolving function scope across two components", async () => {
-        let parseFn = LexerParser.getLexerParserFn(new Map(), defaultExecutionOptions);
-
-        let componentMap = await getComponentDefinitionMap("/doesnt/matter");
-        componentMap.forEach(comp => {
-            comp.scripts = comp.scripts.map(script => {
-                script.uri = path.join("scripts/", path.parse(script.uri).base);
-                return script;
-            });
-        });
-
         let componentToResolve = componentMap.get("ExtendedComponent");
         let componentScopeResolver = new brs.parser.ComponentScopeResolver(componentMap, parseFn);
         let statements = await componentScopeResolver.resolve(componentToResolve);
@@ -55,6 +61,22 @@ describe("ComponentScopeResolver", () => {
         expect(statementNames).toEqual(["test", "test2"]);
 
         let testStatement = statements.find(s => s.name.text === "test");
+        let testStatementCount = testStatement.func.body.statements.length;
+        expect(testStatementCount).toEqual(2);
+    });
+
+    test("resolving function scope with multiple scripts", async () => {
+        let componentToResolve = componentMap.get("ExtendedComponent2");
+        let componentScopeResolver = new brs.parser.ComponentScopeResolver(componentMap, parseFn);
+        let statements = await componentScopeResolver.resolve(componentToResolve);
+        expect(statements).toBeDefined();
+        expect(statements.length).toBe(5);
+
+        let statementNames = statements.map(s => s.name.text);
+        // Statements are in a specific order
+        expect(statementNames).toEqual(["test", "util1", "util_y", "test2", "util_x"]);
+
+        let testStatement = statements.find(s => s.name.text === "util1");
         let testStatementCount = testStatement.func.body.statements.length;
         expect(testStatementCount).toEqual(2);
     });
